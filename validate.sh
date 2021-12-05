53c39a07ca508de4fdc3fd1e84a60c3e57ccc2a4
Gabriel Cohen <94457453+Goobensio@users.noreply.github.com>
2021-12-05 22:51:19 +0200

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -50,13 +50,13 @@ echo ">> Checking out $GITHUB_PAGES_BRANCH branch from $GITHUB_PAGES_REPO"
 cd /tmp/helm/publish
 mkdir -p "$HOME/.ssh"
 ssh-keyscan -H github.com >> "$HOME/.ssh/known_hosts"
-git clone -b "$GITHUB_PAGES_BRANCH" "git@github.com:$GITHUB_PAGES_REPO.git" .
+sudo git clone -b "$GITHUB_PAGES_BRANCH" "git@github.com:$GITHUB_PAGES_REPO.git" .
 
-alias helm=/tmp/helm/bin/linux-amd64/helm
+sudo alias helm=/tmp/helm/bin/linux-amd64/helm
 
 echo '>> Building charts and comparing with labels...'
 sudo find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; do
-  sudo chart_name="`basename "$chart"`"
+  chart_name="`basename "$chart"`"
   echo $chart_name
   for label in $LABELS; do
   if [ $label == $chart_name ]; then
