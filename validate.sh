6b3a6c84b5eebbacad4b5c708d94eafd0d4be76d
Gabriel Cohen <94457453+Goobensio@users.noreply.github.com>
2021-12-06 15:17:44 +0200

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -52,9 +52,9 @@ cd /tmp/helm/publish
 mkdir -p "$HOME/.ssh"
 ssh-keyscan -H github.com >> "$HOME/.ssh/known_hosts"
 git clone -b "$GITHUB_PAGES_BRANCH" "git@github.com:$GITHUB_PAGES_REPO.git" #GITHUB_PAGES_REPO
-sudo alias helm="/tmp/helm/bin/linux-amd64/helm"
+alias helm="/tmp/helm/bin/linux-amd64/helm"
 #cd operator/
-ls "$HELM_CHARTS_SOURCE"
+#ls "$HELM_CHARTS_SOURCE"
 
 echo '>> Building charts and comparing with labels...'
 sudo find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; do
