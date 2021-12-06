3bca7890d222ea0a7013c265d26f633bc680d15a
Gabriel Cohen <94457453+Goobensio@users.noreply.github.com>
2021-12-06 15:52:48 +0200

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -55,6 +55,7 @@ mkdir -p "$HOME/.ssh"
 #git clone -b "gh-pages" "git@github.com:rookout/helm-charts.git" #GITHUB_PAGES_REPO
 git clone -b "gh-pages" "https://github.com/Rookout/helm-charts.git" #GITHUB_PAGES_REPO
 alias helm="/tmp/helm/bin/linux-amd64/helm"
+cd /helm-charts/
 #cd operator/
 #ls "$HELM_CHARTS_SOURCE"
 
