5528abda903db1b0ec915a3f0cb6c3c3d4d31a48
Gabriel Cohen <gavriel@rookout.com>
2021-12-07 18:38:05 +0200

    changed

--- publish.sh
+++ publish.sh
@@ -1,37 +1,13 @@
 #!/bin/sh
+set -e
+set -o pipefail
 
 WORKING_DIRECTORY="$PWD"
-GITHUB_PAGES_REPO=$1
-GITHUB_BRANCH=$2
-GITHUB_USERNAME=$3
-GITHUB_ACTIONS_REPO=$4
-GITHUB_ACTIONS_RUN_ID=$5
 
-[ "$1" ] || {
+[ "$GITHUB_PAGES_REPO" ] || {
   echo "ERROR: Environment variable GITHUB_PAGES_REPO is required"
   exit 1
 }
-
-[ "$2" ] || {
-  echo "ERROR: Environment variable GITHUB_BRANCH is required"
-  exit 1
-}
-
-[ "$3" ] || {
-  echo "ERROR: Environment variable GITHUB_USERNAME is required"
-  exit 1
-}
-
-[ "$4" ] || {
-  echo "ERROR: Environment variable GITHUB_ACTIONS_REPO is required"
-  exit 1
-}
-
-[ "$5" ] || {
-  echo "ERROR: Environment variable GITHUB_ACTIONS_RUN_ID is required"
-  exit 1
-}
-
 [ -z "$GITHUB_PAGES_BRANCH" ] && GITHUB_PAGES_BRANCH=gh-pages
 [ -z "$HELM_CHARTS_SOURCE" ] && HELM_CHARTS_SOURCE="$WORKING_DIRECTORY/charts"
 [ -d "$HELM_CHARTS_SOURCE" ] || {
@@ -39,8 +15,8 @@ GITHUB_ACTIONS_RUN_ID=$5
   exit 1
 }
 [ -z "$HELM_VERSION" ] && HELM_VERSION=2.8.1
-[ "$GITHUB_BRANCH" ] || {
-  echo "ERROR: Environment variable GITHUB_BRANCH is required"
+[ "$CIRCLE_BRANCH" ] || {
+  echo "ERROR: Environment variable CIRCLE_BRANCH is required"
   exit 1
 }
 
@@ -48,50 +24,50 @@ echo "GITHUB_PAGES_REPO=$GITHUB_PAGES_REPO"
 echo "GITHUB_PAGES_BRANCH=$GITHUB_PAGES_BRANCH"
 echo "HELM_CHARTS_SOURCE=$HELM_CHARTS_SOURCE"
 echo "HELM_VERSION=$HELM_VERSION"
-echo "GITHUB_BRANCH=$GITHUB_BRANCH"
-echo "GITHUB_ACTIONS_REPO=$GITHUB_ACTIONS_REPO"
-echo "GITHUB_ACTIONS_RUN_ID=$GITHUB_ACTIONS_RUN_ID"
+echo "CIRCLE_BRANCH=$CIRCLE_BRANCH"
+
+echo '>> Prepare...'
+mkdir -p /tmp/helm/bin
+mkdir -p /tmp/helm/publish
+apk update
+apk add ca-certificates git openssh
+
+echo '>> Installing Helm...'
+cd /tmp/helm/bin
+wget "https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
+tar -zxf "helm-v${HELM_VERSION}-linux-amd64.tar.gz"
+chmod +x linux-amd64/helm
+alias helm=/tmp/helm/bin/linux-amd64/helm
+helm version -c
+helm init -c
 
 echo ">> Checking out $GITHUB_PAGES_BRANCH branch from $GITHUB_PAGES_REPO"
 cd /tmp/helm/publish
 mkdir -p "$HOME/.ssh"
-ls $HOME/.ssh/
-cat $HOME/.ssh/known_hosts
 ssh-keyscan -H github.com >> "$HOME/.ssh/known_hosts"
-sleep 5
-ls $HOME/.ssh/
-cat $HOME/.ssh/known_hosts
-git clone -b "${GITHUB_PAGES_BRANCH}" "https://github.com/${GITHUB_PAGES_REPO}.git"
-alias helm="/tmp/helm/bin/linux-amd64/helm"
-cd helm-charts/
+git clone -b "$GITHUB_PAGES_BRANCH" "git@github.com:$GITHUB_PAGES_REPO.git" .
 
 echo '>> Building charts...'
-sudo find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; do
-  chart_name="`basename "$chart"`"
-  echo ">>> fetching chart $chart_name version"
-  chart_version=$(cat $chart/Chart.yaml | grep -oE "version:\s[0-9]+\.[0-9]+\.[0-9]+" | grep -oE "[0-9]+\.[0-9]+\.[0-9]+")
+find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; do
   echo ">>> helm lint $chart"
   helm lint "$chart"
+  chart_name="`basename "$chart"`"
   echo ">>> helm package -d $chart_name $chart"
   mkdir -p "$chart_name"
   helm package -d "$chart_name" "$chart"
 done
-
 echo '>>> helm repo index'
 helm repo index .
-if [ "$GITHUB_BRANCH" != "refs/heads/master" ]; then
+
+if [ "$CIRCLE_BRANCH" != "master" ]; then
   echo "Current branch is not master and do not publish"
   exit 0
 fi
+
 echo ">> Publishing to $GITHUB_PAGES_BRANCH branch of $GITHUB_PAGES_REPO"
-git config user.email "${GITHUB_USERNAME}@users.noreply.github.com"
-git config user.name Github-Actions-CI
-git config --list
-git config user.name
-git config user.email
+git config user.email "$CIRCLE_USERNAME@users.noreply.github.com"
+git config user.name CircleCI
 git add .
 git status
-echo "Message to commit: Published by github actions https://github.com/${GITHUB_ACTIONS_REPO}/actions/runs/${GITHUB_ACTIONS_RUN_ID}"
-git commit -m "Published by github actions https://github.com/${GITHUB_ACTIONS_REPO}/actions/runs/${GITHUB_ACTIONS_RUN_ID}" #$CIRCLE_BUILD_URL"
-git status
+git commit -m "Published by CircleCI $CIRCLE_BUILD_URL"
 git push origin "$GITHUB_PAGES_BRANCH"
