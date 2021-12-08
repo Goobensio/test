72c7946f0550330d28908ca5581bc5316217459d
Gabriel Cohen <94457453+Goobensio@users.noreply.github.com>
2021-12-08 12:52:18 +0200

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -6,6 +6,7 @@ GITHUB_BRANCH=$2
 GITHUB_USERNAME=$3
 GITHUB_ACTIONS_REPO=$4
 GITHUB_ACTIONS_RUN_ID=$5
+GH_TOKEN=$6
 
 [ "$1" ] || {
   echo "ERROR: Environment variable GITHUB_PAGES_REPO is required"
@@ -64,6 +65,7 @@ git config user.name
 git config user.email
 ssh -T git@github.com
 sleep 2
+echo "$GH_TOKEN"
 echo "@@@@@@@@"
 #ssh-keyscan -H github.com >> "$HOME/.ssh/known_hosts"
 ls -al ~/.ssh
@@ -75,24 +77,24 @@ ssh -T git@github.com
 # cat $HOME/.ssh/known_hosts
 # sleep 3
 #cat $HOME/.ssh/known_hosts
+echo "@@@@@@@@!"
 #git clone -b "${GITHUB_PAGES_BRANCH}" "https://github.com/${GITHUB_PAGES_REPO}.git"
 #git clone -b "${GITHUB_PAGES_BRANCH}" "git@github.com:${GITHUB_PAGES_REPO}.git"
 #sudo git clone -b "${GITHUB_PAGES_BRANCH}" "git@github.com:Goobensio/test.git"
 git clone -b "${GITHUB_PAGES_BRANCH}" "https://github.com/Goobensio/test.git"
+echo "@@@@@@@@!"
 alias helm="/tmp/helm/bin/linux-amd64/helm"
 cd test/
 #cd helm-charts/
-git config user.name
-git config user.email
-echo "@@@@@@@@"
 git config user.email "${GITHUB_USERNAME}@users.noreply.github.com"
 git config user.name Github-Actions-CI
-echo "@@@@@@@@"
 git config user.name
 git config user.email
 sleep 2
 ssh -T git@github.com
 
+
+echo "-------------------------------------"
 echo '>> Building charts...'
 sudo find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; do
   chart_name="`basename "$chart"`"
@@ -111,6 +113,7 @@ if [ "$GITHUB_BRANCH" != "refs/heads/master" ]; then
   echo "Current branch is not master and do not publish"
   exit 0
 fi
+echo "-------------------------------------"
 echo ">> Publishing to $GITHUB_PAGES_BRANCH branch of $GITHUB_PAGES_REPO"
 git config user.email "${GITHUB_USERNAME}@users.noreply.github.com"
 git config user.name Github-Actions-CI
